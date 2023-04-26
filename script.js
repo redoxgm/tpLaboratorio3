2002dcf39faa286baec48fc4a0d5e04c750ce2a2
German Maldonado <germanmaldonado962@gmail.com>
2023-04-26 19:00:08 -0300

    se agrego la seccion 'sobre mi' en el html y se realizo la funcionalidad del boton seguir

---
+++ script.js
@@ -0,0 +1,10 @@
+let btnSeguir =document.getElementById("btnSeguir").addEventListener('click',function(){
+    if(this.value === "Seguir"){
+        this.value = "Dejar de Seguir"
+    }else{
+        this.value = "Seguir"
+    }
+    
+})
+
+
