996825007049033e11ecc9223f6a4286849e7dc6
German Maldonado <germanmaldonado962@gmail.com>
2023-04-26 20:32:23 -0300

    se agrego la funcion del boton de seguir y la validacion de usuario

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-let btnSeguir =document.getElementById("btnSeguir").addEventListener('click',function(){
-    if(this.value === "Seguir"){
-        this.value = "Dejar de Seguir"
+
+
+function comprobarUsuario(){
+    let txtUsuario = document.getElementById("txtUsuario").value;
+    let cantidad = txtUsuario.length
+    let mensajeError ="Debe ingresar un usuario"
+
+    if(a != 0){
+        txtUsuario;
+       
     }else{
-        this.value = "Seguir"
+        mensajeError;
     }
+}
+
+function seguir(){
+    document.getElementById("btnSeguir").addEventListener('click',function(){
+        if(this.value === "Seguir"){
+            this.value = "Dejar de Seguir"
+        }else{
+            this.value = "Seguir"
+        }
+        
+    })
+}
+
+function botonMeGusta(){
+
+    let btnLike =document.getElementById("btnLike").value;
+    let contadorLike = document.getElementById("contadorLike").innerText;
+   
+  
+       
+       
+
     
-})
+}
+
+seguir()
+document.getElementById("btnLike").addEventListener('click',botonMeGusta)
+
+
+
+
 
 
