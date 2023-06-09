e0c6b4214e99cccc18626b2129e9c74437e4f767
German Maldonado <germanmaldonado962@gmail.com>
2023-06-09 17:05:19 -0300

    se modifico logica de comentarios,todas las funcionalidades echas

--- script.js
+++ script.js
@@ -13,9 +13,21 @@ const app = Vue.createApp({
 
             descripcion: '¡Hola a todos! Soy Jane, y actualmente vivo en la vibrante y emocionante ciudad de Tokio,Japón. Me encanta capturar la esencia de la vida urbana a través de mi lente, explorandoel constraste entre la arquitectura moderna y las tradiciones centenarias que convivenen esta metrópolis única. Desde rascacielos deslumbrantes y calles bulliciosas hastatemplos serenos y jardines tranquilos, encuentro inspiracion en cada rincon de esta increible ciudad. ',
             btnSeguir: "Seguir",
+            bandera: true,
+            posteo: "https://i.imgur.com/bq9db41.jpeg",
+
+            numeroLike: 200,
+            disableButton: false,
+            comentario1: "juan Perez: ¡que buena Foto!",
+            comentario2: "Sofia Paz: tengo que volver algun dia",
+
             usuario: '',
+            comentario: '',
+            nuevoComentario: [],
+            bComentar: false,
 
-
+            mostrarError: false,
+            mostrarBtn: false
 
         }
     },
@@ -28,14 +40,45 @@ const app = Vue.createApp({
                 this.btnSeguir = "Seguir"
             }
 
+        },
+        numLike() {
+            this.disableButton = true
+            this.numeroLike += 1
+
+            if (this.numeroLike > 201) {
+                this.disableButton = false
+            }
 
         },
+        agregarComentario() {
+            this.bComentar = false
+            this.mostrarError = false
+            this.mostrarBtn = false
+
+            if (this.usuario != "" && this.comentario != "") {
+                this.bComentar = true
+                this.mostrarBtn = true
+                this.nuevoComentario.push({
+
+                    usuario: this.usuario,
+                    comentario: this.comentario
+                }),
+
+                    this.usuario = ''
+                this.comentario = ''
+            }
+            else {
+                this.mostrarError = true
+            }
+        },
+        eliminarComentario(id) {
+            const index = this.nuevoComentario.findIndex(comentario => comentario.id === id)
+            if (index !== -1) {
+                this.nuevoComentario.splice(index, 1)
+            }
+        }
 
     }
-})
+}
 
-
-
-
-
-
+)
