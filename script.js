01060660a9e7f2d0cf23741b0e48b5924c6f1a12
Matias Maldonado <katamaldonado03@gmail.com>
2023-06-13 17:31:19 -0300

    cambios en el boton eliminar y en Css

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
                 'FechaNacimiento : 12/04/1996',
                 'Telefono : 918-936-4569',
                 'Ciudad: tokyo,Japon',
-                'Trabajando en : https://canon.jp'],
+                'Trabajando en : Cannon JP'],//https://canon.jp
 
             descripcion: '¡Hola a todos! Soy Jane, y actualmente vivo en la vibrante y emocionante ciudad de Tokio,Japón. Me encanta capturar la esencia de la vida urbana a través de mi lente, explorandoel constraste entre la arquitectura moderna y las tradiciones centenarias que convivenen esta metrópolis única. Desde rascacielos deslumbrantes y calles bulliciosas hastatemplos serenos y jardines tranquilos, encuentro inspiracion en cada rincon de esta increible ciudad. ',
             btnSeguir: "Seguir",
@@ -46,8 +46,11 @@ const app = Vue.createApp({
             this.numeroLike += 1
 
             if (this.numeroLike > 201) {
+                
                 this.disableButton = false
             }
+            
+            
 
         },
         agregarComentario() {
@@ -72,10 +75,13 @@ const app = Vue.createApp({
             }
         },
         eliminarComentario(id) {
+            
             const index = this.nuevoComentario.findIndex(comentario => comentario.id === id)
             if (index !== -1) {
                 this.nuevoComentario.splice(index, 1)
+                
             }
+         
         }
 
     }
